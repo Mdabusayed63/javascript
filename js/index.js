c26e1e274157cdc6bc1334f4afd06dc61bb32df7
Mdabusayed63 <rafathasan368@gmail.com>
2022-12-07 13:30:51 +0600

    utydfghe

---
+++ js/index.js
@@ -0,0 +1,77 @@
+// var a = 10;
+// var y = 12.5;
+// var abc= "mehrin";
+// document.write("a:" +a);
+
+// document.write("<p style='color:red;'>This is p tag</p>");
+// abc = parseInt(abc);
+// var studendFristName= prompt("Enter your frist Name");
+// var studentLastName= prompt("Enter your Last Name");
+// document.write(studendFristName + studentLastName);
+// var fristNumber = prompt("enter frist number:");
+// var secendNumber = prompt("enter Secend Number");
+// document.write(Number (fristNumber) + Number (secendNumber));
+// document.write(Number(prompt("Enter frist Number")) + Number(prompt("Enter last Number")))
+// document.writeln( 
+//     "String concatenation: " +
+//     studentFristName.concat(" " + studentLastName) +
+//     "<br/>"
+// );
+// document.write("Length of Last Name: " + studentLastName.toUpperCase() + "<br/>)");
+// document.writeln( 
+//     "string Lowercase function: " + studentLastName.toUpperCase() + "<br/>"
+// )
+// let a = parseFloat(prompt("Enter frist Number"));
+// let b = parseFloat(prompt("Enter second Number"));
+
+// document.write(a+b);
+// document.write(a-b);
+// document.write(a*b);
+// document.write(a/b);
+// // document.write(a%b);
+// let a = 10;
+// let b = 20;
+
+// document.write("<br> arithmatic:" + (a+b));
+// b=a++;
+// document.write("<br> a:" + a );
+// // document.write("<br> b:" + b);
+// var fristNumber = 100;
+// var secendNumber = 200;
+
+// if (fristNumber > secendNumber)
+// document.write(fristNumber > secendNumber);
+// else if (fristNumber < secendNumber)
+// document.write(fristNumber<secendNumber);
+// else
+// document.write("N/A");
+
+// var a = 100 ;
+// var b = 200 ;
+
+// var digit = (Number(prompt("Enter any number")));
+// if(digit < 100)
+// document.write("<100");
+// else if(digit <100 )
+// document.write("> 100");
+// else if (digit > 300)
+// document.write( > 300 );
+
+// var fristNumber = 100;
+// var digit = Number (prompt("Enter any Number"));
+// digit > 20
+// ? document.write("<br>> 20 </br>")
+// : digit < 20 ? document.write("<br/> < 20")
+// var showLetter = prompt("Enter String");
+// switch(showLetter){
+//     case 'A' || 
+// }
+// for (var i =0; i<=10; i++){
+//     document.write(i + "<br>");
+// }
+var i = 1;
+while(i<=10){
+    document.write(i +"<br>");
+    i++;
+}
+
